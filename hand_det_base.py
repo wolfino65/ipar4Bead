73600e5cc4eace30cd706d7b667620ffaad7e2be
wolfino65 <wolfino6565@gmail.com>
2025-05-05 12:16:19 +0200

    get values between landmarks with identifications

--- hand_det_base.py
+++ hand_det_base.py
@@ -1,8 +1,17 @@
 import cv2 
 import mediapipe as mp
-import numpy as np 
-import math
-from google.protobuf.json_format import MessageToDict 
+from google.protobuf.json_format import MessageToDict
+
+class LandmarkValues():
+    def __init__(self, distance=None,l1=None,l2=None,hand=None):
+        self.distance = distance
+        self.l1 = l1
+        self.l2 = l2
+        self.hand = hand
+      
+
+    def __repr__(self):
+        return f"LandmarkValues(Dist={self.distance}, l1={self.l1}, l2={self.l2}, hand={self.hand})"
 
 mpHands = mp.solutions.hands 
 hands = mpHands.Hands( 
@@ -27,51 +36,66 @@ def get_invariant_ratio(landmarks, a_idx, b_idx, ref1_idx=0, ref2_idx=9):
     
     return dist_ab / (dist_ref + 1e-6)  # Avoid division by zero
 
+fingertip_indexes = [4, 8, 12, 16, 20] #fingertips and wrist
+wrist_index=0
+thumb_index=4
+
+def get_values_for_hand(landmarks,hand):
+    temp_list=[]
+    for i in fingertip_indexes:
+        temp_thumb=LandmarkValues(hand=hand)
+        temp_wrist=LandmarkValues(hand=hand)
+        landmark_x = int(landmarks[i].x * img.shape[1])
+        landmark_y = int(landmarks[i].y * img.shape[0])
+        wrist_x = int(landmarks[0].x * img.shape[1])
+        wrist_y = int(landmarks[0].y * img.shape[0])
+        thumb_x = int(landmarks[4].x * img.shape[1])
+        thumb_y = int(landmarks[4].y * img.shape[0])
+        cv2.circle(img, (landmark_x, landmark_y), 10, (255, 0, 0), 1)
+        cv2.line(img, (landmark_x, landmark_y), (wrist_x, wrist_y), (0, 255, 0), 1)
+        cv2.line(img, (landmark_x, landmark_y), (thumb_x, thumb_y), (0, 255, 0), 1) #paths betweeen key landmarks
+        thumb_dist=get_invariant_ratio(landmarks, i, thumb_index) * 100
+        wrist_dist=get_invariant_ratio(landmarks, i, wrist_index) * 100
+        thumb_midpoint=((thumb_x + landmark_x) // 2, (thumb_y + landmark_y) // 2)
+        cv2.putText(img, str(int(thumb_dist)), thumb_midpoint, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+        wrist_midpoint=((wrist_x + landmark_x) // 2, (wrist_y + landmark_y) // 2)
+        cv2.putText(img, str(int(wrist_dist)), wrist_midpoint, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+        temp_thumb.distance=thumb_dist
+        temp_wrist.distance=wrist_dist
+        temp_thumb.l1="thumb"
+        temp_wrist.l1="wrist"
+        temp_thumb.l2=i
+        temp_wrist.l2=i
+        temp_list.append(temp_thumb)
+        temp_list.append(temp_wrist)
+        
+    return temp_list
+
+
 while True:     
+    values=[]
     success, img = cap.read() 
     img = cv2.flip(img, 1) 
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
-    results = hands.process(imgRGB) 
+    results = hands.process(imgRGB)
     if results.multi_hand_landmarks: 
-        fingertip_indexes = [4, 8, 12, 16, 20,0]
-        for hand_landmarks in results.multi_hand_landmarks:
-            
-            """mp_drawing.draw_landmarks(
-                img, 
-                hand_landmarks, 
-                mpHands.HAND_CONNECTIONS,
-                mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=1),
-                mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=1))""" #skeleton
-            for i in fingertip_indexes:
-                landmark_x = int(hand_landmarks.landmark[i].x * img.shape[1])
-                landmark_y = int(hand_landmarks.landmark[i].y * img.shape[0])
-                wrist_x = int(hand_landmarks.landmark[0].x * img.shape[1])
-                wrist_y = int(hand_landmarks.landmark[0].y * img.shape[0])
-                thumb_x = int(hand_landmarks.landmark[4].x * img.shape[1])
-                thumb_y = int(hand_landmarks.landmark[4].y * img.shape[0])
-                cv2.circle(img, (landmark_x, landmark_y), 10, (255, 0, 0), 1)
-                cv2.line(img, (landmark_x, landmark_y), (wrist_x, wrist_y), (0, 255, 0), 1)
-                cv2.line(img, (landmark_x, landmark_y), (thumb_x, thumb_y), (0, 255, 0), 1)
-                #dist=math.dist((landmark_x, landmark_y), (thumb_x, thumb_y))
-                dist=get_invariant_ratio(hand_landmarks.landmark, i, 4) * 100
-                thumb_midpoint=((thumb_x + landmark_x) // 2, (thumb_y + landmark_y) // 2)
-                cv2.putText(img, str(int(dist)), thumb_midpoint, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-                wrist_midpoint=((wrist_x + landmark_x) // 2, (wrist_y + landmark_y) // 2)
-                cv2.putText(img, str(int(dist)), wrist_midpoint, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-                for i in results.multi_handedness: 
-                    dict_result = MessageToDict(i)
-                    if 'classification' in dict_result and len(dict_result['classification']) > 0:
-                        label = dict_result['classification'][0].get('label', '')
-                        if label == 'Left': 
-                            l_w_x = int(hand_landmarks.landmark[0].x * img.shape[1])
-                            l_w_y = int(hand_landmarks.landmark[0].y * img.shape[0])
-                            cv2.putText(img, "left", (wrist_x+20,wrist_y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-                        elif label == 'Right': 
-                            cv2.putText(img, "right", (wrist_x+20,wrist_y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-        
-        
-                        
-            
+        for i in results.multi_handedness:
+            dict_result = MessageToDict(i)
+            if 'classification' in dict_result and len(dict_result['classification']) > 0:
+                label = dict_result['classification'][0].get('label', '')
+                if len(results.multi_hand_landmarks) != 2:
+                    if label == 'Left': 
+                        values+=get_values_for_hand(results.multi_hand_landmarks[0].landmark,label)    
+                    if label == 'Right':
+                        values+=get_values_for_hand(results.multi_hand_landmarks[0].landmark,label)
+                elif len(results.multi_hand_landmarks) == 2:
+                    if label == 'Left': 
+                        values+=get_values_for_hand(results.multi_hand_landmarks[0].landmark,label)    
+                    if label == 'Right':
+                        pass 
+                        values+=get_values_for_hand(results.multi_hand_landmarks[1].landmark,label)
+    """for i in values:
+        print(i)"""
     height, width,_ = img.shape
     height*=1.5
     width*=1.5
